e96db7987688aad01fbc7e9bb0944e9fd512ae7b
Timothy Ehimen <tehi0001@student.monash.edu>
2021-08-12 09:58:20 +0200

    Implemented add product and products components

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductCategory} from "../models/product-category";
+import {MatTableDataSource} from "@angular/material/table";
+import {Product} from "../models/product";
 
 @Component({
   selector: 'app-products',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+	dummyData: Product[] = [];
 
-  ngOnInit(): void {
-  }
+	tableDataSource: MatTableDataSource<Product>
+
+	displayedColumns: string[] = ['sn', 'name', 'description', 'dateCreated', 'actions'];
+
+	constructor() {
+
+		for (let i = 0; i < 10; i++) {
+			let category: Product = {
+				id: i + 100,
+				image: "assets/dummies/product.png",
+				name: "This is a test Product (123 x 234)",
+				price: 200.78,
+				stock: 20
+			}
+
+			this.dummyData.push(category);
+		}
+
+		this.tableDataSource = new MatTableDataSource<Product>(this.dummyData);
+	}
+
+	ngOnInit(): void {
+	}
 
 }
